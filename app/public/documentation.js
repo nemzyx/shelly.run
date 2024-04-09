abcf6781bbd95fede78548306e5e76c15cd588a8
not-nullptr <needhelpwithrift@gmail.com>
2024-04-09 16:07:45 +0100

    add drawing to the grid + docs :3

--- app/public/documentation.js
+++ app/public/documentation.js
@@ -41,6 +41,31 @@ const documentationObject = (() => {
         }
       },
       variables: ['hide', 'idle', 'walk', 'run']
+    },
+    grid: {
+      functions: {
+        drawPixel: {
+          desc: "Draws a pixel on the grid. Color can be a color name, hex or RGB value",
+          smpl: ["grid.drawPixel(0, 0, \"black\")"],
+          args: ["x", "y", "color"]
+        },
+        clearPixel: {
+          desc: "Clears a pixel on the grid",
+          smpl: ["grid.clearPixel(0, 0)"],
+          args: ["x", "y"]
+        },
+        writePixels: {
+          desc: "Write an array of pixels to the grid. Faster than running drawPixel multiple times",
+          smpl: [`grid.writePixels([{ x: 0, y: 0, color: "red" }, { x: 1, y: 0, color: "rgb(0, 255, 0)" }])`],
+          args: ["pixels"]
+        },
+        clearScreen: {
+          desc: "Clears the entire grid",
+          smpl: ["grid.clearScreen()"],
+          args: []
+        }
+      },
+      variables: []
     }
   }
 })()
